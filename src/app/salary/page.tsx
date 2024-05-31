598d43e946c85fabc877154fb69faf2b96a3d363
a.atik <a.atik@platingaming.com>
2024-05-31 14:21:02 +0200

    table

--- src/app/salary/page.tsx
+++ src/app/salary/page.tsx
@@ -17,6 +17,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { SalaryTable } from "./_components/salary-table";
 // import {
 //   defaultMetadata,
 //   ogMetadata,
@@ -61,40 +62,7 @@ export default async function SalaryCalculatorPage() {
           );
         })}
         <SalaryFormCard title="Result" className={"sm:col-span-3"}>
-          <Table>
-            <TableCaption>A list of your recent invoices.</TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[100px]">Month</TableHead>
-                <TableHead>Gross Wage</TableHead>
-                <TableHead>SSI Base Amount</TableHead>
-                <TableHead>SSI Base Employee</TableHead>
-                <TableHead>SSI UnEmp</TableHead>
-                <TableHead>Cumulative Income </TableHead>
-                
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {invoices.map((invoice) => (
-                <TableRow key={invoice.invoice}>
-                  <TableCell className="font-medium">
-                    {invoice.invoice}
-                  </TableCell>
-                  <TableCell>{invoice.paymentStatus}</TableCell>
-                  <TableCell>{invoice.paymentMethod}</TableCell>
-                  <TableCell className="text-right">
-                    {invoice.totalAmount}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell colSpan={3}>Total</TableCell>
-                <TableCell className="text-right">$2,500.00</TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
+          <SalaryTable/>
         </SalaryFormCard>
       </div>
     </>
